def03ae97cd13f62917edf010f9ef9ca6e93b6f2
vividu22 <ipilorget.ledantec@gmail.com>
2026-03-19 13:10:23 +0100

    d

--- réel/index.php
+++ réel/index.php
@@ -1,20 +1,66 @@
 <!DOCTYPE html>
 <html>
+<head>
+    <meta charset="UTF-8">
+    <title>Mini Loup-Garou</title>
+</head>
 <body>
-  <!-- ton HTML PHP ici -->
-  <?php echo "Bonjour " . $user; ?>
+    <h1>Mini Loup-Garou – Exemple Temps Réel</h1>
 
-  <script src="/socket.io/socket.io.js"></script>
-  <script>
-    const socket = io('http://localhost:3000');
+    <label>Pseudo :</label>
+    <input type="text" id="pseudo">
+    <button onclick="joinGame()">Rejoindre</button>
 
-    socket.on('connect', () => {
-      console.log('Connecté :', socket.id);
-    });
+    <h2>Joueurs connectés :</h2>
+    <ul id="players"></ul>
 
-    socket.on('message', (data) => {
-      console.log(data);
-    });
-  </script>
+    <h2>Vote :sudo mysql -u root -p </h2>
+    <select id="voteTarget"></select>
+    <button onclick="sendVote()">Voter</button>
+
+    <h3>Résultat en direct :</h3>
+    <div id="results"></div>
+
+    <!-- Socket.io client -->
+    <script src="http://192.168.1.21:3000/socket.io/socket.io.js"></script>
+
+    <script>
+        const socket = io("http://192.168.1.21:3000");
+        let pseudo = "";
+
+        function joinGame() {
+            pseudo = document.getElementById("pseudo").value;
+            socket.emit("join", pseudo);
+        }
+
+        socket.on("players", (list) => {
+            const ul = document.getElementById("players");
+            const select = document.getElementById("voteTarget");
+
+            ul.innerHTML = "";
+            select.innerHTML = "";
+
+            for (let id in list) {
+                let li = document.createElement("li");
+                li.textContent = list[id];
+                ul.appendChild(li);
+
+                let opt = document.createElement("option");
+                opt.value = list[id];
+                opt.textContent = list[id];
+                select.appendChild(opt);
+            }
+        });
+
+        function sendVote() {
+            let target = document.getElementById("voteTarget").value;
+            socket.emit("vote", target);
+        }
+
+        socket.on("vote-update", (data) => {
+            document.getElementById("results").innerHTML +=
+                `<p>${data.voter} a voté pour ${data.target}</p>`;
+        });
+    </script>
 </body>
 </html>
--- réel/server.js
+++ réel/server.js
@@ -35,7 +35,7 @@ io.on("connection", (socket) => {
 });
 
 http.listen(3000, () => {
-    console.log("Serveur Socket.io lancé sur http://IP:3000");
+    console.log("Serveur Socket.io lancé sur http://localhost:3000");
 });
 
 
