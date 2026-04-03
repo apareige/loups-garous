4c79aea182e5407a7119de25bbf510b283a78721
vividu22 <ipilorget.ledantec@gmail.com>
2026-04-03 11:22:54 +0200

    s

--- réel/connexion_admin/script.js
+++ réel/connexion_admin/script.js
@@ -82,7 +82,7 @@ function validate() {
   if (pin === CORRECT_CODE) {
     setStatus('Code Accepté', 'success');
     setTimeout(() => {
-      window.location.href = 'admin/admin.html';
+      window.location.href = '../acceuil_admin/admin.php';
     }, 600);
   } else {
     setStatus('Code Incorrect', 'fail');
