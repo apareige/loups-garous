c52249fa59c41e9d47061ed9ab6f0bf8569f2787
vividu22 <ipilorget.ledantec@gmail.com>
2026-04-07 18:21:37 +0200

    a

---
+++ réel/sock/socket.js
@@ -0,0 +1,36 @@
+const socket = io("http://192.168.1.21:3000");
+        let pseudo = "";
+
+        function joinGame() {
+            pseudo = document.getElementById("pseudo").value;
+            socket.emit("join", pseudo);
+        }
+
+        socket.on("players", (list) => {
+            const ul = document.getElementById("players");
+            const select = document.getElementById("voteTarget");
+
+            ul.innerHTML = "";
+            select.innerHTML = "";
+
+            for (let id in list) {
+                let li = document.createElement("li");
+                li.textContent = list[id];
+                ul.appendChild(li);
+
+                let opt = document.createElement("option");
+                opt.value = list[id];
+                opt.textContent = list[id];
+                select.appendChild(opt);
+            }
+        });
+
+        function sendVote() {
+            let target = document.getElementById("voteTarget").value;
+            socket.emit("vote", target);
+        }
+
+        socket.on("vote-update", (data) => {
+            document.getElementById("results").innerHTML +=
+                `<p>${data.voter} a voté pour ${data.target}</p>`;
+        });
