169c9dd7711248475ddd625ef8b66bdfd98ceb39
YouwenHTZ <yheitz.ledantec@gmail.com>
2026-04-10 10:50:17 +0200

    Update script.js

--- réel/acceuil_admin/assets/js/script.js
+++ réel/acceuil_admin/assets/js/script.js
@@ -1,1 +1,7 @@
-dcdvff
+function generer() {
+  let code = "";
+  for (let i = 0; i < 4; i++) { // Code a 4 chiffres
+    code += Math.floor(Math.random() * 10);
+  }
+  document.getElementById("code").textContent = code;
+}
